a6661de49887c14747b609a9a64c8683254c1ae7
rubotscript <pajarsultan2@gmail.com>
2025-12-06 15:59:21 +0700

    'fixloader'

---
+++ RubotHub.lua
@@ -0,0 +1,7 @@
+local Games = {
+  [6701277882] = "https://raw.githubusercontent.com/rubotscript/roblox-script/refs/heads/main/FishIt/main.lua" -- Fish It
+}
+local URL = Games[game.GameId]
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
